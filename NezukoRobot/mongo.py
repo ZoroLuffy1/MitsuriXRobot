e92157b54816677f0ce7cd7f8160fd8c3fc456f0
Hiro-Kun <92907462+Harshit-Kun@users.noreply.github.com>
2022-02-01 14:42:37 +0530

    Update mongo.py

--- NezukoRobot/mongo.py
+++ NezukoRobot/mongo.py
@@ -1,5 +1,7 @@
 import asyncio
 import sys
+import logging as log
+
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
@@ -16,7 +18,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["VainBot"]
+db = client["NezukoRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
