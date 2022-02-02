c4d1b847f5e0289d67a1b761c3fbc5fa3ca4145a
Hiro-Kun <92907462+Harshit-Kun@users.noreply.github.com>
2022-02-02 08:38:59 +0530

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -224,29 +224,19 @@ def start(update: Update, context: CallbackContext):
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [
-                       [
-                    InlineKeyboardButton(
-                                text=gs(chat.id, "add_bot_to_group_button"),
-                                url="t.me/NezukoXRobot?startgroup=new",
-                            ),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                                text=gs(chat.id, "about_button"),
-                                callback_data="Siesta_",
-                        ),
-                        InlineKeyboardButton(
-                            text=gs(chat.id, "inline_button"),
-                            switch_inline_query_current_chat="",
-                        ),
-                    ],
-                    [
-                        InlineKeyboardButton(
+                        [
+                            InlineKeyboardButton(
+                                text=gs(chat.id, "add_bot_to_group_button"), url="t.me/NezukoXRobot?startgroup=new"),
+                        ],
+                        [
+                            InlineKeyboardButton(text=gs(chat.id, "about_button"), callback_data="siesta_"),
+                            InlineKeyboardButton(text=gs(chat.id, "inline_button"), switch_inline_query_current_chat=""),
+                        ],
+                        [
+                            InlineKeyboardButton(
                             text=gs(chat.id, "help_button"), callback_data="help_back"
-                        ),
-                   
+                        ]
                     ]
-                    
                 ),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
