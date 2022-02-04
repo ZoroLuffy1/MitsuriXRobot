f504986ffe6536d847e2e70a91d90894aa8b79e3
Harshit-Kun <Harshit-Kun@sakuraunion.net>
2022-02-04 15:19:19 +0000

    Automated code formatting
    Signed-off-by: aryazakaria01 <aryazakaria01@sakuraunion.net>

--- SiestaRobot/modules/anime.py
+++ SiestaRobot/modules/anime.py
@@ -856,18 +856,21 @@ def animequotes(update: Update, context: CallbackContext):
     reply_photo(random.choice(QUOTES_IMG))
 
 
-def watchorderx(_,message):
-	anime =  message.text.replace(message.text.split(' ')[0], '')
-	res = requests.get(f'https://chiaki.site/?/tools/autocomplete_series&term={anime}').json()
-	data = None
-	id_ = res[0]['id']
-	res_ = requests.get(f'https://chiaki.site/?/tools/watch_order/id/{id_}').text
-	soup = BeautifulSoup(res_ , 'html.parser')
-	anime_names = soup.find_all('span' , class_='wo_title')
-	for x in anime_names:
-		data = f"{data}\n{x.text}" if data else x.text
-	message.reply_text(f'Watchorder of {anime}: \n```{data}```')
-    
+def watchorderx(_, message):
+    anime = message.text.replace(message.text.split(" ")[0], "")
+    res = requests.get(
+        f"https://chiaki.site/?/tools/autocomplete_series&term={anime}"
+    ).json()
+    data = None
+    id_ = res[0]["id"]
+    res_ = requests.get(f"https://chiaki.site/?/tools/watch_order/id/{id_}").text
+    soup = BeautifulSoup(res_, "html.parser")
+    anime_names = soup.find_all("span", class_="wo_title")
+    for x in anime_names:
+        data = f"{data}\n{x.text}" if data else x.text
+    message.reply_text(f"Watchorder of {anime}: \n```{data}```")
+
+
 def helps(chat):
     return gs(chat, "anime_help")
 
