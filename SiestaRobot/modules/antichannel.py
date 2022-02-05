738ed4d2ba9fbff7e4d877ca40b0d22e23a97d22
Hiro-Kun <92907462+Harshit-Kun@users.noreply.github.com>
2022-02-05 13:55:31 +0530

    Update antichannel.py

--- SiestaRobot/modules/antichannel.py
+++ SiestaRobot/modules/antichannel.py
@@ -6,13 +6,7 @@ from telegram.ext import CallbackContext
 
 from SiestaRobot.modules.helper_funcs.decorators import siestacmd, siestamsg
 from SiestaRobot.modules.helper_funcs.channel_mode import user_admin, AdminPerms
-from SiestaRobot.modules.sql.antichannel_sql import (
-    antichannel_status,
-    disable_antichannel,
-    enable_antichannel,
-)
-from SiestaRobot.modules.language import gs
-
+from SiestaRobot.modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
 
 @siestacmd(command="antichannelmode", group=100)
 @user_admin(AdminPerms.CAN_RESTRICT_MEMBERS)
@@ -24,23 +18,15 @@ def set_antichannel(update: Update, context: CallbackContext):
         s = args[0].lower()
         if s in ["yes", "on"]:
             enable_antichannel(chat.id)
-            message.reply_html(
-                text=gs(chat.id, "active_antichannel").format(html.escape(chat.title))
-            )
+            message.reply_html("Enabled antichannel in {}".format(html.escape(chat.title)))
         elif s in ["off", "no"]:
             disable_antichannel(chat.id)
-            message.reply_html(
-                text=gs(chat.id, "disable_antichannel").format(html.escape(chat.title))
-            )
+            message.reply_html("Disabled antichannel in {}".format(html.escape(chat.title)))
         else:
-            message.reply_text(text=gs(chat.id, "invalid_antichannel").format(s))
+            message.reply_text("Unrecognized arguments {}".format(s))
         return
     message.reply_html(
-        text=gs(chat.id, "status_antichannel").format(
-            antichannel_status(chat.id), html.escape(chat.title)
-        )
-    )
-
+        "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
 @siestamsg(Filters.chat_type.groups, group=110)
 def eliminate_channel(update: Update, context: CallbackContext):
@@ -49,18 +35,23 @@ def eliminate_channel(update: Update, context: CallbackContext):
     bot = context.bot
     if not antichannel_status(chat.id):
         return
-    if (
-        message.sender_chat
-        and message.sender_chat.type == "channel"
-        and not message.is_automatic_forward
-    ):
+    if message.sender_chat and message.sender_chat.type == "channel" and not message.is_automatic_forward:
         message.delete()
         sender_chat = message.sender_chat
         bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
+        
+__help__ = """
+──「 Anti-Channels 」──
 
+    ⚠️ WARNING ⚠️
 
-def helps(chat):
-    return gs(chat, "antichannel_help")
+*IF YOU USE THIS MODE, THE RESULT IS IN THE GROUP FOREVER YOU CAN'T CHAT USING THE CHANNEL*
 
+Anti Channel Mode is a mode to automatically ban users who chat using Channels. 
+This command can only be used by *Admins*.
+
+❂ /antichannelmode <'on'/'yes'> *:* enables anti-channel-mode
+❂ /antichannelmode <'off'/'no'> *:* disabled anti-channel-mode
+"""
 
 __mod_name__ = "Anti-Channel"
